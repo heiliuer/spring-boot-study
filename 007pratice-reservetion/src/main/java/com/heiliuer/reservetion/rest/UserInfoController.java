c313408732d9a1965996e932560ba4ec6e268e87
heiliuer <eee>
2017-05-14 00:02:26 +0800

    fixed

--- 007pratice-reservetion/src/main/java/com/heiliuer/reservetion/rest/UserInfoController.java
+++ 007pratice-reservetion/src/main/java/com/heiliuer/reservetion/rest/UserInfoController.java
@@ -53,8 +53,6 @@ public class UserInfoController {
 
         logger.info(new Gson().toJson(login));
 
-        userInfoModelDao.findAll()
-
         if ("admin".equalsIgnoreCase(login.getUsername()) && "admin".equalsIgnoreCase(login.getPassword())) {
             session.setAttribute(SESSION_KEY_LOGGED, true);
             return ResponseEntity.ok(new JsonResult(JsonResult.STATUS_OK, "登录成功"));
--- 007pratice-reservetion/src/main/java/com/heiliuer/reservetion/utils/Utils.java
+++ 007pratice-reservetion/src/main/java/com/heiliuer/reservetion/utils/Utils.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
  * Created by hao.wang on 2017-5-12.
  */
 public class Utils {
+    public static final int PERMISSION_ADMIN = 1;
+
     public static boolean isLogged(HttpServletRequest request) {
         Boolean isLogged = (Boolean) request.getSession().getAttribute(Constants.SESSION_KEY_LOGGED);
         return isLogged == null ? false : isLogged;
